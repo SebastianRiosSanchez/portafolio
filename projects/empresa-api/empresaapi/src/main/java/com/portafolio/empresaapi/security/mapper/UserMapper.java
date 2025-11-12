efff26083e038d21512ee2d6c3773b4d7ac23653
Sebastian Sanchez <sebastianrios47@gmail.com>
2025-11-12 16:25:46 -0500

    Creation of userEntityToUserResponseDtoById method on UserMapper

--- projects/empresa-api/empresaapi/src/main/java/com/portafolio/empresaapi/security/mapper/UserMapper.java
+++ projects/empresa-api/empresaapi/src/main/java/com/portafolio/empresaapi/security/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.portafolio.empresaapi.security.mapper;
 
+import com.portafolio.empresaapi.security.dto.RoleResponseDto;
 import com.portafolio.empresaapi.security.dto.UserResponseDto;
 import com.portafolio.empresaapi.security.model.RoleEntity;
 import com.portafolio.empresaapi.security.model.UserEntity;
@@ -20,6 +21,24 @@ public class UserMapper {
                         userEntity.getRoles().stream().map(RoleEntity::getName).collect(Collectors.toSet())
                         : null)
                 .build();
+    }
+
+    public static UserResponseDto userEntityToUserResponseDtoById(UserEntity entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        UserResponseDto dto = new UserResponseDto();
+        dto.setId(entity.getId());
+        dto.setFirstname(entity.getFirstname());
+        dto.setLastname(entity.getLastname());
+        dto.setUserName(entity.getUsername());
+        dto.setIsEnable(entity.getIsEnable());
+        dto.setIsDeleted(entity.getIsDeleted());
+        dto.setRoles(
+                entity.getRoles().stream().map(RoleEntity::getName).collect(Collectors.toSet())
+        );
+        return dto;
     }
 
 }
